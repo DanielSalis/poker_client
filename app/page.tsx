7ebdee638197ec36cdc5af1564626506448ee691
DanielSalis <danielsalis98@gmail.com>
2024-12-24 22:09:26 -0300

    feat: add shadcn dialog

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,36 @@
-import Image from "next/image";
-import background from "./background.png"
+import background from "./background.png";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 
 export default function Home() {
   return (
-    <div className=" w-[100%] h-[100%] flex flex-col justify-center items-center" style={{
-      backgroundImage: `url(${background.src})`,
-      backgroundSize: "cover",
-      backgroundPosition: "center",
-      backgroundRepeat: "no-repeat",
-      }}>
-      <div className=" bg-red-600 w-100 h-100">
-        teste
-      </div>
+    <div
+      className=" w-[100%] h-[100%] flex flex-col justify-center items-center"
+      style={{
+        backgroundImage: `url(${background.src})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+      }}
+    >
+      <Dialog open>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Are you absolutely sure?</DialogTitle>
+            <DialogDescription>
+              This action cannot be undone. This will permanently delete your
+              account and remove your data from our servers.
+            </DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
     </div>
   );
 }
