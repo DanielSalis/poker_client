9f41ef92e2361498da413d1779aae6cce9255737
DanielSalis <danielsalis98@gmail.com>
2025-01-02 10:09:54 -0300

    feat: add action buttons

--- app/room/[id]/page.tsx
+++ app/room/[id]/page.tsx
@@ -5,6 +5,7 @@ import { useParams } from "next/navigation";
 import { useEffect, useState } from "react";
 import { createConsumer } from "@rails/actioncable";
 import "./style.css";
+import { Button } from "@/components/ui/button";
 
 interface IRoom {
   data: {
@@ -36,6 +37,7 @@ interface IPlayer {
   last_action: string | null;
   created_at: string;
   updated_at: string;
+  username: string
 }
 
 const RoomId = () => {
@@ -78,13 +80,7 @@ const RoomId = () => {
         received(data: any) {
           console.log("Received data:", data);
           if (data.action?.name === "join" && data.player_data) {
-            setRoom((prevRoom) => {
-              if (!prevRoom) return null;
-              return {
-                ...prevRoom,
-                players: [...prevRoom.players, data.player_data],
-              };
-            });
+              fetchRoom()
           } else if (data.action?.name === "delete" && data.action.player_id) {
             setRoom((prevRoom) => {
               if (!prevRoom) return null;
@@ -126,11 +122,42 @@ const RoomId = () => {
     return () => {
       window.removeEventListener("beforeunload", handleBeforeUnload);
     };
-  }, [id]);
+  }, [id, room]);
+
+  const handleAction = async (endpoint: string, method: string = "POST") => {
+    const player = JSON.parse(sessionStorage.getItem("player") as string);
+    try {
+      const response = await fetch(`http://localhost:3000/rooms/${id}/${endpoint}`, {
+        method,
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({player_id: player.id})
+      });
+
+      if (!response.ok) {
+        throw new Error(`Failed to call ${endpoint}: ${response.statusText}`);
+      }
+
+      console.log(`Success: ${endpoint}`);
+    } catch (error) {
+      console.error(`Error: ${error}`);
+    }
+  };
 
   return (
-    <div className="room-container">
+    <div className="flex flex-col items-center w-[100%] h-[100%] p-8">
       <h1>Room {id}</h1>
+      <div className="flex justify-center space-x-4 mb-4">
+        {room?.data.status === "waiting" && (
+          <Button variant="default" onClick={() => handleAction("start")}>
+            Start
+          </Button>
+        )}
+        <Button variant="destructive" onClick={() => handleAction("leave")}>
+          Leave
+        </Button>
+      </div>
       <div className="table">
         <div className="community-cards">
           {room && room.data.comunity_cards.length > 0 ? (
@@ -149,10 +176,34 @@ const RoomId = () => {
         {room?.players.map((player, index) => (
           <div key={player.id} className={`player player-${index + 1}`}>
             <span>
-              {index + 1} - Chips: {player.chips}
+              {player.username}
+            </span>
+            <span>
+            Chips: {player.chips}
             </span>
           </div>
         ))}
+      </div>
+
+      <div className="flex justify-center space-x-4 mt-auto">
+        <Button
+          variant="default"
+          onClick={() => handleAction("action", "POST")}
+        >
+          Check
+        </Button>
+        <Button
+          variant="default"
+          onClick={() => handleAction("action", "POST")}
+        >
+          Raise
+        </Button>
+        <Button
+          variant="secondary"
+          onClick={() => handleAction("action", "POST")}
+        >
+          Fold
+        </Button>
       </div>
     </div>
   );
